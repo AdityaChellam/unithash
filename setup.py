801b798555c42b5f020cbebbc59fb5c5c8ccdd6e
adchella <adchella@cisco.com>
2021-04-19 02:42:53 +0530

    Long description update

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name='unithash',
     packages=find_packages(include=['unithash']),
-    version='0.1.2',
+    version='1.1.5',
     description='Hashing Algorithm | Python',
-    long_description='Compute the unit hash digest of a string by segmenting the string into blocks of a specified size and recursively computing the sum of all digits in each block.\nIn order to use this hashing algorithm in your code, do a pip install for the library.\n\n\tpip install unithash\n\nAdditional ',
+    long_description='Compute the unit hash digest of a string by segmenting the string into blocks of a specified size and recursively computing the sum of all digits in each block.\nIn order to use this hashing algorithm in your code, do a pip install for the library.\n\n\tpip install unithash\n\n',
     long_description_content_type="text/markdown",
     author='Aditya Chellam',
     license='MIT',
