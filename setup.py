2f1cbc1aeaf30feb56792a411debab67c062c968
Aditya Chellam <AdityaChellam@users.noreply.github.com>
2021-04-19 12:16:00 +0530

    Update setup.py
    version update 1.2.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='unithash',
     packages=find_packages(include=['unithash']),
-    version='1.1.5',
+    version='1.2.5',
     description='Hashing Algorithm | Python',
     long_description='Compute the unit hash digest of a string by segmenting the string into blocks of a specified size and recursively computing the sum of all digits in each block.\nIn order to use this hashing algorithm in your code, do a pip install for the library.\n\n\tpip install unithash\n\n',
     long_description_content_type="text/markdown",
@@ -13,7 +13,7 @@ setup(
     project_urls={
         "Documentation": "https://www.codeofcoffee.com/2021/04/creating-my-own-python-library-and.html",
     },
-    download_url = 'https://github.com/AdityaChellam/unithash/archive/refs/tags/v_115.tar.gz',
+    download_url = 'https://github.com/AdityaChellam/unithash/archive/refs/tags/v_125.tar.gz',
     keywords = ['HASHING','UNITSUM'],
     install_requires=[],
     setup_requires=['pytest-runner'],
